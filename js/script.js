0927b5601f17c5d33c4e7a88b792709607fbc40f
MariaMaj59 <maj.mari59@gmail.com>
2023-04-25 16:56:41 +0500

    initial

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function openMenu(){
+    document.getElementById("sidebar").classList.toggle('active');
+    document.getElementById("burger").classList.toggle('active');
+    document.getElementById("cross").classList.toggle('active');
+}
